f25f49e291f68aac034e8365ebef54d22fbadff0
Your NameBrayan <bdarocag@gmail.com>
2020-08-28 12:20:49 -0500

    Lectura de la asignación, decidí usar el mismo formato del ejemplo, y a partir de este modifique los nombres, además de usar la funcion solve(), la cual fue sugerida.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## El siguiente script, almacena en caché la inversa de una matriz
 
-## Write a short comment describing this function
+## makeCacheMatrix construye un objeto de tipo matriz
 
 makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(solve) m <<- solve
+  getinv <- function() m
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 
 }
 
 
-## Write a short comment describing this function
+## CacheSolve calcula la inversa de la matriz devuelta por la funcion anterior
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## Devuelve una matriz que es la inversa de 'x'
+  m <- x$getinv()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinv(m)
+  m
 }
